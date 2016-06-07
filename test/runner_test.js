43c0d089787b714e377302b006fddf53d9e4a292
Gabriel Jülke <gabriel.juelke@sectornord.de>
2016-06-07 10:31:51 +0200

    updated tests

--- test/runner_test.js
+++ test/runner_test.js
@@ -7,13 +7,10 @@ describe('Runner', function() {
 	var runner = require('../lib/runner');
 
 	it('should detect if PHPUnit is installed', function(done) {
-		var sys      = sinon.mock().never();
-		var watcher  = sinon.mock(watchr).expects('watch').once();
-		var exec     = sinon.stub().callsArgWith(1, {}, 'PHPUnit');
-		var sut      = new runner(sys, watchr, exec);
+		var watcher  = sinon.mock(watchr).expects('start').once();
+		var sut      = new runner();
 
-		sut.watch('/path');
-		assert.ok(exec.calledOnce);
+		sut.start();
 		assert.equal('phpunit --version', exec.getCall(0).args[0]);
 		assert.ok(watcher.calledOnce);
 		watchr.watch.restore();
@@ -21,13 +18,12 @@ describe('Runner', function() {
 	});
 
 	it('should not watch directory if PHPUnit is not available', function(done) {
-		var sys      = sinon.mock().never();
-		var watcher  = sinon.mock(watchr).expects('watch').never();
+		var watcher  = sinon.mock(watchr).expects('start').never();
 		var response = sinon.stub('NOT', 'match').withArgs(/^PHPUnit/).returns(false);
 		var exec     = sinon.stub().withArgs('phpunit --version').returns({}, response);
-		var sut = new runner(sys, watchr, exec);
+		var sut = new runner();
 
-		sut.watch('/path');
+		sut.start();
 		assert.ok(exec.calledOnce);
 		assert.equal('phpunit --version', exec.getCall(0).args[0]);
 		watchr.watch.restore();
@@ -35,15 +31,15 @@ describe('Runner', function() {
 	});
 
 	it('should default configuration to colors', function(done) {
-		var sut = new runner({}, {}, {});
-		assert.equal(sut.getSettings(), '--colors');
+		var sut = new runner();
+		assert.equal(sut.getOptions(), '--colors');
 		done();
 	});
 
 	it('can set configuration for PHPUnit', function(done) {
-		var sut = new runner({}, {}, {});
+		var sut = new runner();
 		sut.setup('--strict');
-		assert.equal(sut.getSettings(), '--strict');
+		assert.equal(sut.getOptions(), '--strict');
 		done();
 	});
 });
